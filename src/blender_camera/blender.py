db9b6730ae86a0dc7a96c573abf77d2914708ea0
Leonard Hansing <lhansing92@gmail.com>
2025-09-20 19:00:59 +0000

    changes

--- src/blender_camera/blender.py
+++ src/blender_camera/blender.py
@@ -6,16 +6,7 @@ import aiohttp
 import tempfile
 
 
-def _save_camera_to_tmp_file(camera: Camera) -> str:
-    """Saves camera data to a temporary JSON file and returns the file path."""
-
-    tmp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".json")
-    with open(tmp_file.name, "w") as f:
-        f.write(camera.model_dump_json())
-    return tmp_file.name
-
-
-async def _load_scene(url: str) -> str:
+async def _load_blend_file(url: str) -> str:
     """Downloads a scene file from a URL and returns the local file path."""
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
@@ -28,15 +19,25 @@ async def _load_scene(url: str) -> str:
     return tmp_file.name
 
 
-async def render_pointcloud(camera: Camera) -> bytes:
+def _save_camera_to_tmp_file(camera: Camera) -> str:
+    """Saves camera data to a temporary JSON file and returns the file path."""
+
+    tmp_file = tempfile.NamedTemporaryFile(delete=False, suffix=".json")
+    with open(tmp_file.name, "w") as f:
+        f.write(camera.model_dump_json())
+    return tmp_file.name
+
+
+async def render_pointcloud(blend_url: str, camera: Camera) -> bytes:
     """Renders a point cloud in ply format and returns the binary data."""
+    blend_file_path = await _load_blend_file(blend_url)
     json_path = _save_camera_to_tmp_file(camera)
-    blend_file_path = await _load_scene(camera.scene_url)
 
     try:
         subprocess.run(
             [
                 "blender",
+                blend_file_path,
                 "--background",
                 "--python",
                 "this_script.py",
@@ -46,22 +47,21 @@ async def render_pointcloud(camera: Camera) -> bytes:
             ]
         )
     finally:
+        os.remove(blend_file_path)
         os.remove(json_path)
 
-        if os.path.exists(blend_file_path):
-            os.remove(blend_file_path)
 
-
-async def render_image(camera: Camera) -> bytes:
+async def render_image(blend_url: str, camera: Camera) -> bytes:
     """Renders an image in PNG format and returns the binary data."""
+    blend_file_path = await _load_blend_file(blend_url)
     json_path = _save_camera_to_tmp_file(camera)
-    blend_file_path = await _load_scene(camera.scene_url)
 
     try:
         output_path = "/tmp/rendered_image.png"
         subprocess.run(
             [
                 "blender",
+                blend_file_path,
                 "--background",
                 "--python",
                 "this_script.py",
@@ -73,10 +73,8 @@ async def render_image(camera: Camera) -> bytes:
             ]
         )
     finally:
+        os.remove(blend_file_path)
         os.remove(json_path)
-
-        if os.path.exists(blend_file_path):
-            os.remove(blend_file_path)
 
         if os.path.exists(output_path):
             os.remove(output_path)
