559950dda9de53319ff56dfdcae3dfc9c86d46db
Leonard Hansing <lhansing92@gmail.com>
2025-10-02 18:57:11 +0000

    add intrinsics to camera

--- src/blender_camera/models/camera_model.py
+++ src/blender_camera/models/camera_model.py
@@ -1,5 +1,6 @@
 from uuid import uuid4
 
+from blender_camera.models.camera_intrinsics import CameraIntrinsics
 from blender_camera.models.entities.camera import Camera
 from blender_camera.models.entity_model import EntityModel
 from blender_camera.models.id import Id
@@ -13,9 +14,17 @@ class CameraModel:
     def get_cameras(self) -> list[Camera]:
         return self.entity_model.get_entities_by_type(Camera)
 
-    def create_camera(self, pose: Pose | None = None) -> Camera:
+    def create_camera(
+        self,
+        pose: Pose | None = None,
+        camera_intrinsics: CameraIntrinsics | None = None,
+    ) -> Camera:
         return self.entity_model.add_entity(
-            Camera(id=str(uuid4()), pose=pose or [0, 0, 0, 0, 0, 0])
+            Camera(
+                id=str(uuid4()),
+                pose=pose or [0, 0, 0, 0, 0, 0],
+                camera_intrinsics=camera_intrinsics,
+            )
         )
 
     def delete_camera(self, camera_id: Id):
--- src/blender_camera/models/entities/camera.py
+++ src/blender_camera/models/entities/camera.py
@@ -1,6 +1,8 @@
+from blender_camera.models.camera_intrinsics import CameraIntrinsics
 from blender_camera.models.entities.entity import Entity
 from blender_camera.models.pose import Pose
 
 
 class Camera(Entity):
     pose: Pose
+    camera_intrinsics: CameraIntrinsics | None
